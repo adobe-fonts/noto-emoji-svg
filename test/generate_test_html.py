c377bfba1d5369427e87dddfcb6c92ce7ca5a36b
Miguel Sousa <msousa@adobe.com>
2019-04-18 23:21:20 -0700

    Test HTML file regenerated from Unicode 11 list and without country flags

--- test/generate_test_html.py
+++ test/generate_test_html.py
@@ -8,10 +8,14 @@ import os
 import sys
 from shutil import copyfile
 
+REG_IND_LETTR = ('1F1E6 1F1E7 1F1E8 1F1E9 1F1EA 1F1EB 1F1EC 1F1ED '
+                 '1F1EE 1F1EF 1F1F0 1F1F1 1F1F2 1F1F3 1F1F4 1F1F5 '
+                 '1F1F6 1F1F7 1F1F8 1F1F9 1F1FA 1F1FB 1F1FC 1F1FD '
+                 '1F1FE 1F1FF').split()
 
 FILE_PREFIX = 'emoji_u'
 
-TEST_DIR = os.path.abspath(os.path.dirname(__file__))
+TEST_DIR = os.path.dirname(os.path.abspath(__file__))
 EMOJI_TEST_FILE = os.path.join(TEST_DIR, 'emoji-test.txt')
 TEST_HEADER_FILE = os.path.join(TEST_DIR, 'test_header.html')
 TEST_FILE = os.path.join(TEST_DIR, '../test.html')
@@ -36,7 +40,7 @@ def append_to_file(fpath, data):
 def parse_emoji_test_file():
     """
     Parses Unicode's 'emoji-test.txt' file (available from
-    http://unicode.org/Public/emoji/12.0/emoji-test.txt)
+    http://unicode.org/Public/emoji/M.m/ where 'M.m' is the version number)
     and returns a list of code points.
     """
     with io.open(EMOJI_TEST_FILE, encoding='utf-8') as fp:
@@ -47,8 +51,10 @@ def parse_emoji_test_file():
         line = line.strip()
         if not line or line.startswith('#'):
             continue
-        codepoints = line.split(';')[0].strip().split()
-        cdpts_list.append(codepoints)
+        codepoints, status = line.split(';')
+        if 'unqualified' in status or 'non-fully-qualified' in status:
+            continue
+        cdpts_list.append(codepoints.strip().split())
     return cdpts_list
 
 
@@ -57,8 +63,8 @@ def main(args=None):
     copyfile(TEST_HEADER_FILE, TEST_FILE)
 
     for i, cps in enumerate(cdpts_list, 1):
-        # XXX ignore variation sequences for now
-        if 'FE0F' in cps:
+        # XXX skip country flags for now
+        if cps[0] in REG_IND_LETTR:
             continue
         cps_html = ''.join(['&#x{};'.format(cp) for cp in cps])
         filename = FILE_PREFIX + '_'.join(cps).lower()
